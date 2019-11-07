de3eec4065091cfaa4b5bb05546a5bc03a4f5b8c
Jose Luis Álvarez Bretaña <jalvarezbretana@nautilus11.danielcastelao.org>
2019-11-07 10:37:23 +0100

    Terminado

---
+++ Boletin8_1/src/boletin8_1/Boletin8_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin8_1;
+
+/**
+ *
+ * @author jalvarezbretana
+ */
+public class Boletin8_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calcular8_1 obx = new Calcular8_1();
+        obx.calculo();
+    }
+
+}
---
+++ Boletin8_1/src/boletin8_1/Calcular8_1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin8_1;
+import java.util.Scanner;
+/**
+ *
+ * @author jalvarezbretana
+ */
+public class Calcular8_1 {
+
+    Scanner teclado = new Scanner(System.in);
+    private float fin;
+
+    public void calculo() {
+        System.out.println("Inserte un valor numérico enteiro.");
+        float respuesta = teclado.nextFloat();
+        if (respuesta < 500) {
+            fin = respuesta + respuesta * 50 / 100;
+        }
+        if (respuesta > 500 && respuesta < 1000) {
+            fin = respuesta + respuesta * 7 / 100;
+        }
+        if (respuesta > 1000 && respuesta < 5000) {
+            fin = respuesta + respuesta * 15 / 100;
+        }
+        if (respuesta > 5000) {
+            fin = respuesta - respuesta * 3 / 100;
+        }
+        System.out.println("A cantidade é " + respuesta + ".");
+        System.out.println("O incremento é " + (fin - respuesta) + ".");
+        System.out.println("A cantidade final é " + fin + ".");
+
+    }
+}
